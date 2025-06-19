f2e379cfb0f761c3733857818129547d0a1e86a1
antochi <adrian.constantin.antochi@gmail.com>
2025-06-19 20:56:44 +0300

    added task filtering by status

--- before_oop.py
+++ before_oop.py
@@ -55,6 +55,22 @@ def remove_task(title):
             tasks.remove(task)
             print("Task '{}' has been deleted!".format(title))
 
+def list_tasks_by_status(status):
+    # Return all tasks that have the given status
+    if status in valid_statuses:
+        # Method 1:
+        # filtered_tasks = []
+        # for task in tasks:
+        #     if task["status"] == status:
+        #         filtered_tasks.append(task)
+
+        # Method 2
+        filtered_tasks = list(filter(lambda t: t["status"] == status, tasks))
+        return filtered_tasks
+    else:
+        print("Status '{}' invalid!".format(status))
+        return []
+
 def print_tasks():
     for task in tasks:
         print(task)
@@ -63,10 +79,16 @@ def print_tasks():
 add_task("Pickles", "Buy cucumbers and salt, and mustard seeds.", "in_progress")
 add_task("Dishes", "Wash the dishes.", "in_progress")
 add_task("Workout", "Go to the gym.", "done")
+add_task("Work", "Annotate data.", "in_progress")
+add_task("School", "Receive diploma.", "pending")
+add_task("Life", "Get married.", "pending")
+add_task("Life2", "Relax a bit.", "in_progress")
+
 add_task("Workout", "Look for another gym.", "pending")
 
 update_status("Dishes", "done")
 update_status("Play", "done")
 remove_task("Dishes")
 
-print_tasks()
+# print_tasks()
+print(list_tasks_by_status("in_progress"))
